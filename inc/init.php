737aa5f3cc1df07f0e2ec083199e3ecae066abb8
James Koster <james@jameskoster.co.uk>
2014-08-18 09:53:19 +0100

    structure

--- inc/init.php
+++ inc/init.php
@@ -8,46 +8,46 @@
 /**
  * Implement the Custom Header feature.
  */
-require get_template_directory() . '/inc/custom-header.php';
+require get_template_directory() . '/inc/customizer/custom-header.php';
 
 /**
  * Setup.
  * Enqueue styles, register widget regions, etc.
  */
-require get_template_directory() . '/inc/setup.php';
+require get_template_directory() . '/inc/functions/setup.php';
 
 /**
  * Hooks.
  * Template tags hooked into actions.
  */
-require get_template_directory() . '/inc/hooks.php';
+require get_template_directory() . '/inc/structure/hooks.php';
 
 /**
  * Custom template tags for this theme.
  */
-require get_template_directory() . '/inc/template-tags.php';
+require get_template_directory() . '/inc/structure/template-tags.php';
 
 /**
  * Custom functions that act independently of the theme templates.
  */
-require get_template_directory() . '/inc/extras.php';
+require get_template_directory() . '/inc/functions/extras.php';
 
 /**
  * Customizer additions.
  */
 if ( current_theme_supports( 'storefront-customizer-settings' ) ) {
-	require get_template_directory() . '/inc/customizer.php';
+	require get_template_directory() . '/inc/customizer/customizer.php';
 }
 
 /**
  * Load Jetpack compatibility file.
  */
-require get_template_directory() . '/inc/jetpack.php';
+require get_template_directory() . '/inc/jetpack/jetpack.php';
 
 /**
  * Load welcome screen
  */
-require get_template_directory() . '/inc/welcome-screen.php';
+require get_template_directory() . '/inc/admin/welcome-screen.php';
 
 /**
  * Load Jetpack WooCommerce file.
