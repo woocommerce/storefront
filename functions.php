f3324ce9af3ad4384a834539b4912eaea5d351b9
James Koster <james@jameskoster.co.uk>
2014-08-18 10:19:40 +0100

    child theme note

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * storefront engine room
+ *
+ * @package storefront
+ */
+
+/**
+ * Initialize all the things.
+ */
+require get_template_directory() . '/inc/init.php';
+
+/**
+ * Note: Do not add any custom code here. Please use a child theme so that your customizations aren't lost during updates.
+ * http://codex.wordpress.org/Child_Themes
+ */
