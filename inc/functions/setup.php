eecf1b9a063c4170b339aefd7b11199f17b9b696
James Koster <james@jameskoster.co.uk>
2014-09-02 15:37:52 +0100

    minify js

--- inc/functions/setup.php
+++ inc/functions/setup.php
@@ -124,9 +124,9 @@ function storefront_widgets_init() {
 function storefront_scripts() {
 	wp_enqueue_style( 'storefront-style', get_stylesheet_uri(), '', '1.0.0' );
 
-	wp_enqueue_script( 'storefront-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_script( 'storefront-navigation', get_template_directory_uri() . '/js/navigation.min.js', array(), '20120206', true );
 
-	wp_enqueue_script( 'storefront-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'storefront-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
