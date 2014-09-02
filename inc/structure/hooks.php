916e925575674e183113cbfa9cded0ce4c90269d
James Koster <james@jameskoster.co.uk>
2014-09-02 15:40:26 +0100

    spacing

--- inc/structure/hooks.php
+++ inc/structure/hooks.php
@@ -86,7 +86,6 @@ add_action( 'storefront_single_post_after', 'storefront_display_comments', 	10 )
  */
 add_action( 'storefront_page', 'storefront_page_header', 	10 );
 add_action( 'storefront_page', 'storefront_page_content', 	20 );
-
 add_action( 'storefront_page_after', 'storefront_display_comments', 10 );
 
 /**
