f2060437fbd0abdfac0900df86fb3f384d632dd7
James Koster <james@jameskoster.co.uk>
2014-10-15 17:14:13 +0100

    move storefront_wp_title

--- inc/functions/extras.php
+++ inc/functions/extras.php
@@ -34,37 +34,6 @@ function storefront_body_classes( $classes ) {
 }
 
 /**
- * Filters wp_title to print a neat <title> tag based on what is being viewed.
- *
- * @param string $title Default title text for current view.
- * @param string $sep Optional separator.
- * @return string The filtered title.
- */
-function storefront_wp_title( $title, $sep ) {
-	if ( is_feed() ) {
-		return $title;
-	}
-
-	global $page, $paged;
-
-	// Add the blog name
-	$title .= get_bloginfo( 'name', 'display' );
-
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) ) {
-		$title .= " $sep $site_description";
-	}
-
-	// Add a page number if necessary:
-	if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
-		$title .= " $sep " . sprintf( __( 'Page %s', 'storefront' ), max( $paged, $page ) );
-	}
-
-	return $title;
-}
-
-/**
  * Sets the authordata global when viewing an author archive.
  *
  * This provides backwards compatibility with
