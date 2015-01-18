2f72f768a3462a236270df9e2074ba0d31542f23
Shinichi Nishikawa <shinichi.nishikawa@gmail.com>
2015-01-18 17:15:02 +0700

    set ";" position in the double quotation mark.

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
 	<?php
 	do_action( 'storefront_before_header' ); ?>
 
-	<header id="masthead" class="site-header" role="banner" <?php if ( get_header_image() != '' ) { echo 'style="background-image: url(' . esc_url( get_header_image() ) . ')";'; } ?>>
+	<header id="masthead" class="site-header" role="banner" <?php if ( get_header_image() != '' ) { echo 'style="background-image: url(' . esc_url( get_header_image() ) . ');"'; } ?>>
 		<div class="col-full">
 
 			<?php
