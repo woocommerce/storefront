1ce96115ea5fe04367a57f87cc36a25a65005c2b
James Koster <james@jameskoster.co.uk>
2015-02-20 09:54:35 +0000

    review prompt in 'more' control. closes #135

--- inc/customizer/controls/more.php
+++ inc/customizer/controls/more.php
@@ -16,6 +16,13 @@ class More_Storefront_Control extends WP_Customize_Control {
 					printf( __( 'There\'s a range of Storefront extensions available to put additional power in your hands. Check out the %sStorefront%s page in your dashboard for more information.', 'storefront' ), '<a href="' . esc_url( admin_url() . 'themes.php?page=storefront-welcome#add-ons' ) .'">', '</a>' );
 				?>
 			</p>
+
+			<span class="customize-control-title"><?php _e( 'Enjoying Storefront?', 'storefront' ); ?></span>
+			<p>
+				<?php
+					printf( __( 'Why not leave us a review on %sWordPress.org%s?  We\'d really appreciate it!', 'storefront' ), '<a href="https://wordpress.org/themes/storefront">', '</a>' );
+				?>
+			</p>
 		</label>
 		<?php
 	}
