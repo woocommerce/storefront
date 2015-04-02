97507be8388f4381ba2fa3859decf2addcf9b08d
James Koster <james@jameskoster.co.uk>
2015-04-02 10:34:59 +0100

    sorting priorities

--- inc/woocommerce/hooks.php
+++ inc/woocommerce/hooks.php
@@ -35,11 +35,11 @@ add_action( 'woocommerce_after_shop_loop', 			'woocommerce_result_count', 				20
 add_action( 'woocommerce_after_shop_loop', 			'woocommerce_pagination', 					30 );
 add_action( 'woocommerce_after_shop_loop',			'storefront_sorting_wrapper_close',			31 );
 
-add_action( 'woocommerce_before_shop_loop',			'storefront_sorting_wrapper',				19 );
-add_action( 'woocommerce_before_shop_loop', 		'woocommerce_catalog_ordering', 			20 );
-add_action( 'woocommerce_before_shop_loop', 		'woocommerce_result_count', 				30 );
-add_action( 'woocommerce_before_shop_loop', 		'woocommerce_pagination', 					40 );
-add_action( 'woocommerce_before_shop_loop',			'storefront_sorting_wrapper_close',			41 );
+add_action( 'woocommerce_before_shop_loop',			'storefront_sorting_wrapper',				9 );
+add_action( 'woocommerce_before_shop_loop', 		'woocommerce_catalog_ordering', 			10 );
+add_action( 'woocommerce_before_shop_loop', 		'woocommerce_result_count', 				20 );
+add_action( 'woocommerce_before_shop_loop', 		'woocommerce_pagination', 					30 );
+add_action( 'woocommerce_before_shop_loop',			'storefront_sorting_wrapper_close',			31 );
 
 
 
