e8cfc2e3832cc09e6d600833c001ce10461e938f
James Koster <james@jameskoster.co.uk>
2015-06-10 12:15:16 +0100

    galleria. closes #183

--- inc/admin/welcome-screen/sections/child-themes.php
+++ inc/admin/welcome-screen/sections/child-themes.php
@@ -17,14 +17,14 @@
 	<div class="feature-section col two-col">
 
 		<div class="col-1">
-			<img src="<?php echo esc_url( get_template_directory_uri() ) . '/inc/admin/welcome-screen/img/boutique.jpg'; ?>" alt="<?php _e( 'Boutique Child Theme', 'storefront' ); ?>" class="image-50" />
-			<h4><?php _e( 'Boutique', 'storefront' ); ?></h4>
-			<p><?php _e( 'Boutique is a simple, traditionally designed Storefront child theme, ideal for small stores or boutiques. Add your logo, create a unique color scheme and start selling!', 'storefront' ); ?></p>
+			<img src="<?php echo esc_url( get_template_directory_uri() ) . '/inc/admin/welcome-screen/img/galleria.jpg'; ?>" alt="<?php _e( 'Galleria Child Theme', 'storefront' ); ?>" class="image-50" />
+			<h4><?php _e( 'Galleria', 'storefront' ); ?></h4>
+			<p><?php _e( 'Galleria is a Storefront child theme perfect for fashion and design stores. Stylish and minimalist, it gives sites a high class look and keeps products centerstage.', 'storefront' ); ?></p>
 			<p style="margin-bottom: 2.618em;">
-				<?php if ( 'Boutique' != $theme['Name'] ) { ?>
-					<a href="<?php echo esc_url( wp_nonce_url( self_admin_url( 'update.php?action=install-theme&theme=boutique' ), 'install-theme_boutique' ) ); ?>" class="button button-primary"><?php printf( __( 'Install %s now', 'storefront' ), '<span class="screen-reader-text">Boutique</span>' ); ?></a>
+				<?php if ( 'Galleria' != $theme['Name'] ) { ?>
+					<a href="http://www.woothemes.com/products/galleria/" class="button button-primary"><?php printf( __( 'Buy %s now', 'storefront' ), '<span class="screen-reader-text">Galleria</span>' ); ?></a>
 				<?php } ?>
-				<a href="http://www.woothemes.com/products/boutique/" class="button"><?php printf( __( 'Read more %sabout Boutique%s &rarr;', 'storefront' ), '<span class="screen-reader-text">', '</span>' ); ?></a>
+				<a href="http://www.woothemes.com/products/galleria/" class="button"><?php printf( __( 'Read more %sabout Galleria%s &rarr;', 'storefront' ), '<span class="screen-reader-text">', '</span>' ); ?></a>
 			</p>
 		</div>
 
@@ -37,6 +37,18 @@
 					<a href="<?php echo esc_url( wp_nonce_url( self_admin_url( 'update.php?action=install-theme&theme=deli' ), 'install-theme_deli' ) ); ?>" class="button button-primary"><?php printf( __( 'Install %s now', 'storefront' ), '<span class="screen-reader-text">Deli</span>' ); ?></a>
 				<?php } ?>
 				<a href="http://www.woothemes.com/products/deli/" class="button"><?php printf( __( 'Read more %sabout Deli%s &rarr;', 'storefront' ), '<span class="screen-reader-text">', '</span>' ); ?></a>
+			</p>
+		</div>
+
+		<div class="col-1">
+			<img src="<?php echo esc_url( get_template_directory_uri() ) . '/inc/admin/welcome-screen/img/boutique.jpg'; ?>" alt="<?php _e( 'Boutique Child Theme', 'storefront' ); ?>" class="image-50" />
+			<h4><?php _e( 'Boutique', 'storefront' ); ?></h4>
+			<p><?php _e( 'Boutique is a simple, traditionally designed Storefront child theme, ideal for small stores or boutiques. Add your logo, create a unique color scheme and start selling!', 'storefront' ); ?></p>
+			<p style="margin-bottom: 2.618em;">
+				<?php if ( 'Boutique' != $theme['Name'] ) { ?>
+					<a href="<?php echo esc_url( wp_nonce_url( self_admin_url( 'update.php?action=install-theme&theme=boutique' ), 'install-theme_boutique' ) ); ?>" class="button button-primary"><?php printf( __( 'Install %s now', 'storefront' ), '<span class="screen-reader-text">Boutique</span>' ); ?></a>
+				<?php } ?>
+				<a href="http://www.woothemes.com/products/boutique/" class="button"><?php printf( __( 'Read more %sabout Boutique%s &rarr;', 'storefront' ), '<span class="screen-reader-text">', '</span>' ); ?></a>
 			</p>
 		</div>
 
