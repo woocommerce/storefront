fed1182d2f80e343ce533b7719a271cd4d95b90f
James Koster <james@jameskoster.co.uk>
2015-07-09 12:41:45 +0100

    add proshop to welcome screen. closes #192

--- inc/admin/welcome-screen/sections/child-themes.php
+++ inc/admin/welcome-screen/sections/child-themes.php
@@ -17,6 +17,18 @@
 	<div class="feature-section col two-col">
 
 		<div class="col-1">
+			<img src="<?php echo esc_url( get_template_directory_uri() ) . '/inc/admin/welcome-screen/img/proshop.jpg'; ?>" alt="<?php _e( 'Proshop Child Theme', 'storefront' ); ?>" class="image-50" />
+			<h4><?php _e( 'Proshop', 'storefront' ); ?></h4>
+			<p><?php _e( 'Unlock the true potential of your sports clothing and equipment store with ProShop! It\'s metropolitan design provides an active aesthetic giving your store oodles of character.', 'storefront' ); ?></p>
+			<p style="margin-bottom: 2.618em;">
+				<?php if ( 'Proshop' != $theme['Name'] ) { ?>
+					<a href="http://www.woothemes.com/products/proshop/" class="button button-primary"><?php printf( __( 'Buy %s now', 'storefront' ), '<span class="screen-reader-text">Proshop</span>' ); ?></a>
+				<?php } ?>
+				<a href="http://www.woothemes.com/products/proshop/" class="button"><?php printf( __( 'Read more %sabout Proshop%s &rarr;', 'storefront' ), '<span class="screen-reader-text">', '</span>' ); ?></a>
+			</p>
+		</div>
+
+		<div class="col-2 last-feature">
 			<img src="<?php echo esc_url( get_template_directory_uri() ) . '/inc/admin/welcome-screen/img/galleria.jpg'; ?>" alt="<?php _e( 'Galleria Child Theme', 'storefront' ); ?>" class="image-50" />
 			<h4><?php _e( 'Galleria', 'storefront' ); ?></h4>
 			<p><?php _e( 'Galleria is a Storefront child theme perfect for fashion and design stores. Stylish and minimalist, it gives sites a high class look and keeps products centerstage.', 'storefront' ); ?></p>
@@ -25,18 +37,6 @@
 					<a href="http://www.woothemes.com/products/galleria/" class="button button-primary"><?php printf( __( 'Buy %s now', 'storefront' ), '<span class="screen-reader-text">Galleria</span>' ); ?></a>
 				<?php } ?>
 				<a href="http://www.woothemes.com/products/galleria/" class="button"><?php printf( __( 'Read more %sabout Galleria%s &rarr;', 'storefront' ), '<span class="screen-reader-text">', '</span>' ); ?></a>
-			</p>
-		</div>
-
-		<div class="col-2 last-feature">
-			<img src="<?php echo esc_url( get_template_directory_uri() ) . '/inc/admin/welcome-screen/img/deli.jpg'; ?>" alt="<?php _e( 'Deli Child Theme', 'storefront' ); ?>" class="image-50" />
-			<h4><?php _e( 'Deli', 'storefront' ); ?></h4>
-			<p><?php _e( 'Deli features a texturised, earthy design, perfect for stores selling natural, organic or hand made goods.', 'storefront' ); ?></p>
-			<p style="margin-bottom: 2.618em;">
-				<?php if ( 'Deli' != $theme['Name'] ) { ?>
-					<a href="<?php echo esc_url( wp_nonce_url( self_admin_url( 'update.php?action=install-theme&theme=deli' ), 'install-theme_deli' ) ); ?>" class="button button-primary"><?php printf( __( 'Install %s now', 'storefront' ), '<span class="screen-reader-text">Deli</span>' ); ?></a>
-				<?php } ?>
-				<a href="http://www.woothemes.com/products/deli/" class="button"><?php printf( __( 'Read more %sabout Deli%s &rarr;', 'storefront' ), '<span class="screen-reader-text">', '</span>' ); ?></a>
 			</p>
 		</div>
 
@@ -49,6 +49,18 @@
 					<a href="<?php echo esc_url( wp_nonce_url( self_admin_url( 'update.php?action=install-theme&theme=boutique' ), 'install-theme_boutique' ) ); ?>" class="button button-primary"><?php printf( __( 'Install %s now', 'storefront' ), '<span class="screen-reader-text">Boutique</span>' ); ?></a>
 				<?php } ?>
 				<a href="http://www.woothemes.com/products/boutique/" class="button"><?php printf( __( 'Read more %sabout Boutique%s &rarr;', 'storefront' ), '<span class="screen-reader-text">', '</span>' ); ?></a>
+			</p>
+		</div>
+
+		<div class="col-2 last-feature">
+			<img src="<?php echo esc_url( get_template_directory_uri() ) . '/inc/admin/welcome-screen/img/deli.jpg'; ?>" alt="<?php _e( 'Deli Child Theme', 'storefront' ); ?>" class="image-50" />
+			<h4><?php _e( 'Deli', 'storefront' ); ?></h4>
+			<p><?php _e( 'Deli features a texturised, earthy design, perfect for stores selling natural, organic or hand made goods.', 'storefront' ); ?></p>
+			<p style="margin-bottom: 2.618em;">
+				<?php if ( 'Deli' != $theme['Name'] ) { ?>
+					<a href="<?php echo esc_url( wp_nonce_url( self_admin_url( 'update.php?action=install-theme&theme=deli' ), 'install-theme_deli' ) ); ?>" class="button button-primary"><?php printf( __( 'Install %s now', 'storefront' ), '<span class="screen-reader-text">Deli</span>' ); ?></a>
+				<?php } ?>
+				<a href="http://www.woothemes.com/products/deli/" class="button"><?php printf( __( 'Read more %sabout Deli%s &rarr;', 'storefront' ), '<span class="screen-reader-text">', '</span>' ); ?></a>
 			</p>
 		</div>
 
