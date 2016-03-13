2da60f1ca6df2ff81b168fe89111020a2c643bef
Brad Griffin <burleson99@gmail.com>
2016-03-12 18:33:03 -0600

    Add Best Selling Products for 2.0
    Comment added to line 48
    added_some_action_jackson to line 56

--- inc/storefront-template-hooks.php
+++ inc/storefront-template-hooks.php
@@ -45,6 +45,7 @@ add_action( 'storefront_footer', 'storefront_credit',         20 );
  * @see  storefront_featured_products()
  * @see  storefront_popular_products()
  * @see  storefront_on_sale_products()
+ * @see  storefront_best_selling_products()
  */
 add_action( 'homepage', 'storefront_homepage_content',   10 );
 add_action( 'homepage', 'storefront_product_categories', 20 );
@@ -52,6 +53,7 @@ add_action( 'homepage', 'storefront_recent_products',    30 );
 add_action( 'homepage', 'storefront_featured_products',  40 );
 add_action( 'homepage', 'storefront_popular_products',   50 );
 add_action( 'homepage', 'storefront_on_sale_products',   60 );
+add_action( 'homepage', 'storefront_best_selling_prodcuts', 70);
 
 /**
  * Posts
