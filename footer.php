9107d28ad4e172c1b79eef5cffb836ba2633436d
opportus <opportus@gmail.com>
2016-07-27 14:52:43 +0200

    improved semantic html - header and footer fixes - revision1

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 
 	<?php do_action( 'storefront_before_footer' ); ?>
 
-	<footer id="colophon" class="site-footer">
+	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="col-full">
 
 			<?php
--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 	<?php
 	do_action( 'storefront_before_header' ); ?>
 
-	<header id="masthead" class="site-header" style="<?php storefront_header_styles(); ?>">
+	<header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>">
 		<div class="col-full">
 
 			<?php
