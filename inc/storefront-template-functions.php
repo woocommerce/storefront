0222a61cc521dd81f397174d98b38ff5277d650e
Patrick Rauland <BFTrick@users.noreply.github.com>
2016-10-05 09:12:38 -0600

    Update storefront-template-functions.php
    Changing footer credit from WooThemes to WooCommerce

--- inc/storefront-template-functions.php
+++ inc/storefront-template-functions.php
@@ -125,7 +125,7 @@ if ( ! function_exists( 'storefront_credit' ) ) {
 		<div class="site-info">
 			<?php echo esc_html( apply_filters( 'storefront_copyright_text', $content = '&copy; ' . get_bloginfo( 'name' ) . ' ' . date( 'Y' ) ) ); ?>
 			<?php if ( apply_filters( 'storefront_credit_link', true ) ) { ?>
-			<br /> <?php printf( esc_attr__( '%1$s designed by %2$s.', 'storefront' ), 'Storefront', '<a href="http://www.woothemes.com" title="Premium WordPress Themes & Plugins by WooThemes" rel="author">WooThemes</a>' ); ?>
+			<br /> <?php printf( esc_attr__( '%1$s designed by %2$s.', 'storefront' ), 'Storefront', '<a href="http://www.woocommerce.com" title="WooCommerce - The Best eCommerce Platform for WordPress" rel="author">WooCommerce</a>' ); ?>
 			<?php } ?>
 		</div><!-- .site-info -->
 		<?php
