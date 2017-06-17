0eece63e4c433b1ad4d28a06563bd40d729a37e3
Tiago Noronha <hello@tiagonoronha.com>
2017-06-17 09:13:27 +0200

    New hook after body tag: storefront_before_site

--- header.php
+++ header.php
@@ -19,9 +19,11 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+<?php do_action( 'storefront_before_site' ); ?>
+
 <div id="page" class="hfeed site">
-	<?php
-	do_action( 'storefront_before_header' ); ?>
+	<?php do_action( 'storefront_before_header' ); ?>
 
 	<header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>">
 		<div class="col-full">
