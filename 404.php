901fa313cd2b1a38c3d8e9d5b560da199a942b95
Tiago Noronha <hello@tiagonoronha.com>
2017-06-23 12:42:45 +0300

    Localize strings in 404 template

--- 404.php
+++ 404.php
@@ -22,7 +22,7 @@ get_header(); ?>
 					<p><?php esc_html_e( 'Nothing was found at this location. Try searching, or check out the links below.', 'storefront' ); ?></p>
 
 					<?php
-					echo '<section aria-label="Search">';
+					echo '<section aria-label="' . esc_html__( 'Search', 'storefront' ) . '">';
 
 					if ( storefront_is_woocommerce_activated() ) {
 						the_widget( 'WC_Widget_Product_Search' );
@@ -36,33 +36,34 @@ get_header(); ?>
 
 						echo '<div class="fourohfour-columns-2">';
 
-							echo '<section class="col-1" aria-label="Promoted Products">';
+							echo '<section class="col-1" aria-label="' . esc_html__( 'Promoted Products', 'storefront' ) . '">';
 
 								storefront_promoted_products();
 
 							echo '</section>';
 
-							echo '<nav class="col-2" aria-label="Product Categories">';
+							echo '<nav class="col-2" aria-label="' . esc_html__( 'Product Categories', 'storefront' ) . '">';
 
-							echo '<h2>' . esc_html__( 'Product Categories', 'storefront' ) . '</h2>';
+								echo '<h2>' . esc_html__( 'Product Categories', 'storefront' ) . '</h2>';
 
-							the_widget( 'WC_Widget_Product_Categories', array(
-																			'count'		=> 1,
-							) );
+								the_widget( 'WC_Widget_Product_Categories', array(
+									'count' => 1,
+								) );
+
 							echo '</nav>';
 
-							echo '</div>';
+						echo '</div>';
 
-							echo '<section aria-label="Popular Products" >';
+						echo '<section aria-label="' . esc_html__( 'Popular Products', 'storefront' ) . '">';
 
 							echo '<h2>' . esc_html__( 'Popular Products', 'storefront' ) . '</h2>';
 
 							echo storefront_do_shortcode( 'best_selling_products', array(
-								'per_page'  => 4,
-								'columns'   => 4,
+								'per_page' => 4,
+								'columns'  => 4,
 							) );
 
-							echo '</section>';
+						echo '</section>';
 					}
 					?>
 
