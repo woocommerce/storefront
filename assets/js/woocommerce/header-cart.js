4ea0688cabba7e23b6faf3043ac51aec4a2278af
Tiago Noronha <hello@tiagonoronha.com>
2017-07-12 18:17:00 +0100

    Closes #613: Fixes header mini cart scroll effect.

--- assets/js/woocommerce/header-cart.js
+++ assets/js/woocommerce/header-cart.js
@@ -1,6 +1,6 @@
 /**
  * Makes the header cart content scrollable if the height of the dropdown exceeds the window height.
- * Apply the CSS on page load and mouseover. Mouseover is used as items can be added to the cart via ajax and we'll need to recheck.
+ * Mouseover is used as items can be added to the cart via ajax and we'll need to recheck.
  */
 ( function() {
 	if ( document.body.classList.contains( 'woocommerce-cart' ) || document.body.classList.contains( 'woocommerce-checkout' ) || window.innerWidth < 768 || ! document.getElementById( 'site-header-cart' ) ) {
@@ -10,16 +10,10 @@
 	window.addEventListener( 'load', function() {
 		var cart = document.querySelector( '.site-header-cart' );
 
-		var windowHeight  = window.outerHeight,
-			cartBottomPos = cart.querySelector( '.widget_shopping_cart_content' ).getBoundingClientRect().bottom + cart.offsetHeight,
-			cartList      = cart.querySelector( '.cart_list' );
-
-		if ( cartBottomPos > windowHeight ) {
-			cartList.style.maxHeight = '15em';
-			cartList.style.overflowY = 'auto';
-		}
-
 		cart.addEventListener( 'mouseover', function() {
+			var windowHeight  = window.outerHeight,
+				cartBottomPos = this.querySelector( '.widget_shopping_cart_content' ).getBoundingClientRect().bottom + this.offsetHeight,
+				cartList      = this.querySelector( '.cart_list' );
 
 			if ( cartBottomPos > windowHeight ) {
 				cartList.style.maxHeight = '15em';
