b82a8eb5555bc738792a8820d4ff379ef7dd9f9b
Ashish Kumar <ashfame@users.noreply.github.com>
2017-07-24 16:25:33 +0530

    remove invalid parameter passed to remove_action
    remove 4th parameter being passed to remove_action, it only accepts 3

--- inc/woocommerce/storefront-woocommerce-template-hooks.php
+++ inc/woocommerce/storefront-woocommerce-template-hooks.php
@@ -19,7 +19,7 @@
  * @see  woocommerce_breadcrumb()
  * @see  storefront_shop_messages()
  */
-remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb',                   20, 0 );
+remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb',                   20 );
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper',       10 );
 remove_action( 'woocommerce_after_main_content',  'woocommerce_output_content_wrapper_end',   10 );
 remove_action( 'woocommerce_sidebar',             'woocommerce_get_sidebar',                  10 );
