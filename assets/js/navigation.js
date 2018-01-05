3d817f9e160b0f32cf656904cdbfdea494dfb722
valdrinkoshi <valdrin@google.com>
2018-01-05 14:27:51 -0800

    fix spacing

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -5,63 +5,63 @@
  * Also adds a focus class to parent li's for accessibility.
  * Finally adds a class required to reveal the search in the handheld footer bar.
  */
-(function() {
+( function() {
 	// Wait for DOM to be ready.
-	document.addEventListener('DOMContentLoaded', function() {
-		var container = document.getElementById('site-navigation');
-		if (!container) {
+	document.addEventListener( 'DOMContentLoaded', function() {
+		var container = document.getElementById( 'site-navigation' );
+		if ( !container ) {
 			return;
 		}
 
-		var button = container.querySelector('button');
-		if (!button) {
+		var button = container.querySelector( 'button' );
+		if ( !button ) {
 			return;
 		}
 
-		var menu = container.querySelector('ul');
+		var menu = container.querySelector( 'ul' );
 		// Hide menu toggle button if menu is empty and return early.
-		if (!menu) {
+		if ( !menu ) {
 			button.style.display = 'none';
 			return;
 		}
 
-		button.setAttribute('aria-expanded', 'false');
-		menu.setAttribute('aria-expanded', 'false');
-		menu.classList.add('nav-menu');
+		button.setAttribute( 'aria-expanded', 'false' );
+		menu.setAttribute( 'aria-expanded', 'false' );
+		menu.classList.add( 'nav-menu' );
 
-		button.addEventListener('click', function() {
-			container.classList.toggle('toggled');
-			var expanded = container.classList.contains('toggled') ? 'true' : 'false';
-			button.setAttribute('aria-expanded', expanded);
-			menu.setAttribute('aria-expanded', expanded);
-		});
+		button.addEventListener( 'click', function() {
+			container.classList.toggle( 'toggled' );
+			var expanded = container.classList.contains( 'toggled' ) ? 'true' : 'false';
+			button.setAttribute( 'aria-expanded', expanded );
+			menu.setAttribute( 'aria-expanded', expanded );
+		} );
 
 		// Add class to footer search when clicked.
-		document.querySelectorAll('.storefront-handheld-footer-bar .search > a').forEach(function(anchor) {
-			anchor.addEventListener('click', function(event) {
-				anchor.parentElement.classList.toggle('active');
+		document.querySelectorAll( '.storefront-handheld-footer-bar .search > a' ).forEach( function(anchor) {
+			anchor.addEventListener( 'click', function(event) {
+				anchor.parentElement.classList.toggle( 'active' );
 				event.preventDefault();
-			});
-		});
+			} );
+		} );
 
 		// Add focus class to parents of sub-menu anchors.
-		document.querySelectorAll('.site-header .menu-item > a, .site-header .page_item > a, .site-header-cart a').forEach(function(anchor) {
+		document.querySelectorAll( '.site-header .menu-item > a, .site-header .page_item > a, .site-header-cart a' ).forEach( function(anchor) {
 			var li = anchor.parentNode;
-			anchor.addEventListener('focus', function() {
-				li.classList.add('focus');
-			});
-			anchor.addEventListener('blur', function() {
-				li.classList.remove('focus');
-			});
-		});
+			anchor.addEventListener( 'focus', function() {
+				li.classList.add( 'focus' );
+			} );
+			anchor.addEventListener( 'blur', function() {
+				li.classList.remove( 'focus' );
+			} );
+		} );
 
 		// Add an identifying class to dropdowns when on a touch device
 		// This is required to switch the dropdown hiding method from a negative `left` value to `display: none`.
-		if (('ontouchstart' in window || navigator.maxTouchPoints) && window.innerWidth > 767) {
-			document.querySelectorAll('.site-header ul ul, .site-header-cart .widget_shopping_cart').forEach(function(element) {
-				element.classList.add('sub-menu--is-touch-device');
-			});
+		if ( ( 'ontouchstart' in window || navigator.maxTouchPoints ) && window.innerWidth > 767 ) {
+			document.querySelectorAll( '.site-header ul ul, .site-header-cart .widget_shopping_cart' ).forEach( function(element) {
+				element.classList.add( 'sub-menu--is-touch-device' );
+			} );
 		}
-	});
+	} );
 
-})();
+} )();
--- assets/js/woocommerce/checkout.js
+++ assets/js/woocommerce/checkout.js
@@ -1,65 +1,65 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener( 'DOMContentLoaded', function() {
 
 	// Do sticky on scroll
-	document.addEventListener('scroll', stickyPayment);
+	document.addEventListener( 'scroll', stickyPayment );
 	// Do sticky on window resize
-	window.addEventListener('resize', stickyPayment);
+	window.addEventListener( 'resize', stickyPayment );
 
 	/**
 	 * Make the order review element stick to the top of the browser window.
 	 */
 	function stickyPayment() {
-		if (stickyPayment._tick) {
-			cancelAnimationFrame(stickyPayment._tick);
+		if ( stickyPayment._tick ) {
+			cancelAnimationFrame( stickyPayment._tick );
 		}
-		stickyPayment._tick = requestAnimationFrame(function() {
+		stickyPayment._tick = requestAnimationFrame( function() {
 			stickyPayment._tick = null;
 
-			var review = document.querySelector('#order_review');
-			var heading = document.querySelector('#order_review_heading');
-			var checkout = document.querySelector('form.woocommerce-checkout');
-			var details = document.querySelector('#customer_details');
-			if (!heading || !checkout || !details) {
+			var review = document.querySelector( '#order_review' );
+			var heading = document.querySelector( '#order_review_heading' );
+			var checkout = document.querySelector( 'form.woocommerce-checkout' );
+			var details = document.querySelector( '#customer_details' );
+			if ( !heading || !checkout || !details ) {
 				return;
 			}
 
 			var tallestPaymentBox = -1;
 			var currentPaymentBox = -1;
-			document.querySelectorAll('.payment_box').forEach(function(box) {
+			document.querySelectorAll( '.payment_box' ).forEach( function(box) {
 				var boxHeight = box.offsetHeight;
-				tallestPaymentBox = Math.max(tallestPaymentBox, boxHeight);
-				if (box.querySelector('input:checked')) {
+				tallestPaymentBox = Math.max( tallestPaymentBox, boxHeight );
+				if ( box.querySelector( 'input:checked' ) ) {
 					currentPaymentBox = boxHeight;
 				}
-			});
+			} );
 
 			var termsHeight = 0; // If terms aren't being displayed don't include their height in calculations
-			if (document.querySelector('.wc-terms-and-conditions')) {
+			if ( document.querySelector( '.wc-terms-and-conditions' ) ) {
 				termsHeight = 216; // This is static and set by WooCommerce core + 16px margin added by Storefront
 			}
-			var expandedHeight = review.offsetHeight + termsHeight + (tallestPaymentBox - currentPaymentBox + 30);
+			var expandedHeight = review.offsetHeight + termsHeight + ( tallestPaymentBox - currentPaymentBox + 30 );
 
 			// If we're in desktop orientation and the order review column is taller than the customer details column and smaller than the window height
-			if (details.offsetHeight > expandedHeight && window.innerWidth > 768 && window.innerHeight > expandedHeight) {
-				if (document.body.scrollTop > heading.getBoundingClientRect().top) {
+			if ( details.offsetHeight > expandedHeight && window.innerWidth > 768 && window.innerHeight > expandedHeight ) {
+				if ( document.body.scrollTop > heading.getBoundingClientRect().top ) {
 					var paymentWidth = review.offsetWidth;
 					var paymentOffset = checkout.offsetWidth - paymentWidth;
-					review.classList.add('payment-fixed');
+					review.classList.add( 'payment-fixed' );
 					review.style.width = paymentWidth + 'px';
 					// Compute only once rtl.
-					if (review._isRTL === undefined) {
-						review._isRTL = getComputedStyle(review).direction === 'rtl';
+					if ( review._isRTL === undefined ) {
+						review._isRTL = getComputedStyle( review ).direction === 'rtl';
 					}
-					if (review._isRTL) {
+					if ( review._isRTL ) {
 						review.style.marginRight = paymentOffset + 'px';
 					} else {
 						review.style.marginLeft = paymentOffset + 'px';
 					}
 				} else {
-					review.classList.remove('payment-fixed');
-					review.removeAttribute('style');
+					review.classList.remove( 'payment-fixed' );
+					review.removeAttribute( 'style' );
 				}
 			}
-		});
+		} );
 	}
-});
+} );
