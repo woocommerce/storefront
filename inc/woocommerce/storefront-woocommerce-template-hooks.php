c254a57c01c6df91d5e50030419fa3260fc4e074
Tiago Noronha <hello@tiagonoronha.com>
2018-04-13 15:44:40 +0100

    Remove breadcrumb negative margin/padding

--- inc/woocommerce/storefront-woocommerce-template-hooks.php
+++ inc/woocommerce/storefront-woocommerce-template-hooks.php
@@ -29,7 +29,7 @@ remove_action( 'woocommerce_before_shop_loop',    'woocommerce_catalog_ordering'
 add_action( 'woocommerce_before_main_content',    'storefront_before_content',                10 );
 add_action( 'woocommerce_after_main_content',     'storefront_after_content',                 10 );
 add_action( 'storefront_content_top',             'storefront_shop_messages',                 15 );
-add_action( 'storefront_content_top',             'woocommerce_breadcrumb',                   10 );
+add_action( 'storefront_before_content',          'woocommerce_breadcrumb',                   10 );
 
 add_action( 'woocommerce_after_shop_loop',        'storefront_sorting_wrapper',               9 );
 add_action( 'woocommerce_after_shop_loop',        'woocommerce_catalog_ordering',             10 );
