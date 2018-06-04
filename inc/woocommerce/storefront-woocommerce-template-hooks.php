979e2868b1f599c77707bb49ebb72b14bdf0c2e5
Tiago Noronha <hello@tiagonoronha.com>
2018-06-04 15:53:10 +0100

    Move WC homepage sections

--- inc/woocommerce/storefront-woocommerce-template-hooks.php
+++ inc/woocommerce/storefront-woocommerce-template-hooks.php
@@ -6,10 +6,21 @@
  */
 
 /**
- * Styles
+ * Homepage
  *
- * @see  storefront_woocommerce_scripts()
+ * @see  storefront_product_categories()
+ * @see  storefront_recent_products()
+ * @see  storefront_featured_products()
+ * @see  storefront_popular_products()
+ * @see  storefront_on_sale_products()
+ * @see  storefront_best_selling_products()
  */
+add_action( 'homepage', 'storefront_product_categories',    20 );
+add_action( 'homepage', 'storefront_recent_products',       30 );
+add_action( 'homepage', 'storefront_featured_products',     40 );
+add_action( 'homepage', 'storefront_popular_products',      50 );
+add_action( 'homepage', 'storefront_on_sale_products',      60 );
+add_action( 'homepage', 'storefront_best_selling_products', 70 );
 
 /**
  * Layout
