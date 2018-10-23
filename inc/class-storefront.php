f531ea36d61233a34aef54c985bfaf5409712787
Tiago Noronha <hello@tiagonoronha.com>
2018-10-23 16:51:27 +0100

    Use Pointer Events API for consistency across touchscreen devices

--- inc/class-storefront.php
+++ inc/class-storefront.php
@@ -267,6 +267,8 @@ if ( ! class_exists( 'Storefront' ) ) :
 			if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 				wp_enqueue_script( 'comment-reply' );
 			}
+
+			wp_enqueue_script( 'jquery-pep', get_template_directory_uri() . '/assets/js/vendor/pep.min.js', array(), '0.4.3', true );
 		}
 
 		/**
