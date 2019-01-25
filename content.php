c823541113ba2b261725fe19c94583321eea0765
Tiago Noronha <hello@tiagonoronha.com>
2019-01-25 18:26:03 +0000

    Remove past references for `storefront_post_meta()`

--- content.php
+++ content.php
@@ -14,7 +14,6 @@
 	 * Functions hooked in to storefront_loop_post action.
 	 *
 	 * @hooked storefront_post_header          - 10
-	 * @hooked storefront_post_meta            - 20
 	 * @hooked storefront_post_content         - 30
 	 */
 	do_action( 'storefront_loop_post' );
