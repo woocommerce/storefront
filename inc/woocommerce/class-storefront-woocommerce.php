0d8c9ed568f132a6935487ea0e5ce3f723290298
Tiago Noronha <hello@tiagonoronha.com>
2019-02-26 14:51:18 +0000

    Move footer.js enqueue to WC class

--- inc/woocommerce/class-storefront-woocommerce.php
+++ inc/woocommerce/class-storefront-woocommerce.php
@@ -145,6 +145,8 @@ if ( ! class_exists( 'Storefront_WooCommerce' ) ) :
 			wp_register_script( 'storefront-header-cart', get_template_directory_uri() . '/assets/js/woocommerce/header-cart' . $suffix . '.js', array(), $storefront_version, true );
 			wp_enqueue_script( 'storefront-header-cart' );
 
+			wp_enqueue_script( 'storefront-handheld-footer-bar', get_template_directory_uri() . '/assets/js/footer' . $suffix . '.js', array(), $storefront_version, true );
+
 			if ( ! class_exists( 'Storefront_Sticky_Add_to_Cart' ) && is_product() ) {
 				wp_register_script( 'storefront-sticky-add-to-cart', get_template_directory_uri() . '/assets/js/sticky-add-to-cart' . $suffix . '.js', array(), $storefront_version, true );
 			}
