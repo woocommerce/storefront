717cedcff69e9d485b256cdfd606cc745de258b8
Niels Lange <info@nielslange.de>
2020-02-12 11:24:41 +0800

    #1208: Add support for the wp_body_open hook

--- inc/wordpress-shims.php
+++ inc/wordpress-shims.php
@@ -9,7 +9,7 @@ if ( ! function_exists( 'wp_body_open' ) ) {
     /**
      * Adds backwards compatibility for wp_body_open() introduced with WordPress 5.2
      *
-     * @since 2.5.3
+     * @since 2.5.4
      * @see https://developer.wordpress.org/reference/functions/wp_body_open/
      * @return void
      */
