4f5c52e2cba786e1e908cb1cf947cb2046ea467a
haszari <rua.haszard@automattic.com>
2020-02-12 14:21:55 +1300

    move autoprefixer browserslist config to (recommended) .browserslist:
    Update to autoprefixer (9.7.4) caused a build warning, recommending
    moving browserslist config settings to a common location (was previously
    hard-coded in gruntfile).
    
    See browserlist docs for more info:
    https://github.com/browserslist/browserslist

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,13 +10,7 @@ module.exports = function( grunt ) {
 		postcss: {
 			options: {
 				processors: [
-					require( 'autoprefixer' )({
-						browsers: [
-							'> 0.1%',
-							'ie 8',
-							'ie 9'
-						]
-					})
+					require( 'autoprefixer' )()
 				]
 			},
 			dist: {
