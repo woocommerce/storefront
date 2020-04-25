fd78eefc0e9543009071b85ce8efe2f9454c44ec
Niels Lange <info@nielslange.de>
2020-04-25 18:49:05 +0700

    #1330: Implement e2e testing → First draft

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	launch: {
+		headless: true,
+		slowMo: true,
+		devtools: true
+	}
+}
---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	preset: "jest-puppeteer",
+	testMatch: [
+		"**/tests/**/*.test.js"
+	],
+	verbose: true,
+	testTimeout: 100000
+}
