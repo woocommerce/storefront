e518d81126c692f4cdba233e8130e5aa5c62032d
Rua Haszard <rua.haszard@automattic.com>
2020-07-03 11:45:11 +1200

    inconsequential comment commit to see if it triggers CI PR checks

--- e2e/specs/admin.test.js
+++ e2e/specs/admin.test.js
@@ -14,7 +14,7 @@ beforeAll( async () => {
 } );
 
 describe( 'Storefront onboarding', () => {
-	// Work in progress!
+	// Work in progress - testing!
 	it( 'should launch customizer', async () => {
 		await expect( page ).toMatch( 'You are customizing' );
 	} );
