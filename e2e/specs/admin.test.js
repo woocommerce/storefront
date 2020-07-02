bebd7efc40301419c6195fb0048de218b19bce44
Rua Haszard <rua.haszard@automattic.com>
2020-07-02 17:06:34 +1200

    fix various selectors in admin / onboarding test setup

--- e2e/specs/admin.test.js
+++ e2e/specs/admin.test.js
@@ -3,17 +3,17 @@ import {
 	visitAdminPage,
 } from '@wordpress/e2e-test-utils';
 
+beforeAll( async () => {
+	// Log in as admin, switch to dashboard.
+	await switchUserToAdmin();
+	await visitAdminPage( '' );
+
+	// Click the onboarding notice "Let's go" link, wait for customizer.
+	await page.click( '.sf-notice-nux .sf-nux-button' );
+	await page.waitForSelector( '#customize-header-actions input#save' );
+} );
+
 describe( 'Storefront onboarding', () => {
-	beforeAll( async () => {
-		// Log in as admin, switch to dashboard.
-		await switchUserToAdmin();
-		await visitAdminPage( '' );
-
-		// Click the onboarding notice "Let's go" link, wait for customizer.
-		await page.click( '.sf-notice-nux #sf-nux-button' );
-		await page.waitForSelector( '#customize-header-actions button#save' );
-	} );
-
 	// Work in progress!
 	it( 'should launch customizer', async () => {
 		await expect( page ).toMatch( 'You are customizing' );
