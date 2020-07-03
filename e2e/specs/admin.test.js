bfb5b14143013b257c7a9618e98fdf4a79c3c58c
Rua Haszard <rua.haszard@automattic.com>
2020-07-03 14:46:10 +1200

    run storefront customizer setup inside `describe` block + add comment

--- e2e/specs/admin.test.js
+++ e2e/specs/admin.test.js
@@ -3,17 +3,19 @@ import {
 	visitAdminPage,
 } from '@wordpress/e2e-test-utils';
 
-beforeAll( async () => {
-	// Log in as admin, switch to dashboard.
-	await switchUserToAdmin();
-	await visitAdminPage( '' );
-
-	// Click the onboarding notice "Let's go" link, wait for customizer.
-	await page.click( '.sf-notice-nux .sf-nux-button' );
-	await page.waitForSelector( '#customize-header-actions input#save' );
-} );
-
 describe( 'Storefront onboarding', () => {
+	// Enter the customizer via the "Let's go" onboarding link.
+	// Note this assumes that the nux notice is displayed - this may be fragile.
+	beforeAll( async () => {
+		// Log in as admin, switch to dashboard.
+		await switchUserToAdmin();
+		await visitAdminPage( '' );
+
+		// Click the onboarding notice "Let's go" link, wait for customizer.
+		await page.click( '.sf-notice-nux .sf-nux-button' );
+		await page.waitForSelector( '#customize-header-actions input#save' );
+	} );
+
 	// Work in progress!
 	it( 'should launch customizer', async () => {
 		await expect( page ).toMatch( 'You are customizing' );
