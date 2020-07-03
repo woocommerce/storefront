d97c7e02e1f02cbb18df838a43794bec8e91e59b
Rua Haszard <rua.haszard@automattic.com>
2020-07-03 14:41:29 +1200

    speed up e2e:dev slowmo option

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,7 @@
 
 // Launch chromium and run tests at slow speed so can see where things go wrong.
 const launch = process.env.STOREFRONT_E2E_DEV ? {
-	slowMo: 250,
+	slowMo: 50,
 	headless: false,
 	devtools: true,
 } : undefined;
