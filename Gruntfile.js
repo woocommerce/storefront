d867a14db1f3d6b24369d48f507f36e2d5b88929
Albert Juhé Lluveras <contact@albertjuhe.com>
2020-11-11 09:15:50 +0100

    Remove editor dynamic width feature (#1525)
    * Remove editor dynamic width
    
    * Remove babel completely
    
    * Use narrow width in editor

--- Gruntfile.js
+++ Gruntfile.js
@@ -222,7 +222,6 @@ module.exports = function( grunt ) {
 					// main js
 					'assets/js/**/*.js',
 					'!assets/js/**/*.min.js',
-					'!assets/js/editor.js',
 
 					// customizer js
 					'assets/js/customizer/**/*.js',
@@ -241,7 +240,6 @@ module.exports = function( grunt ) {
 					'!assets/js/admin/welcome-screen/**/*.min.js'
 				],
 				tasks: [
-					'babel',
 					'jshint',
 					'uglify'
 				]
@@ -450,16 +448,6 @@ module.exports = function( grunt ) {
 					{ src: './storefront/**' }
 				]
 			}
-		},
-		babel: {
-			options: {
-				presets: ['@wordpress/babel-preset-default']
-			},
-			dist: {
-				files: {
-					'./assets/js/editor.js': './assets/js/src/editor.js'
-				}
-			}
 		}
 	});
 
@@ -476,13 +464,11 @@ module.exports = function( grunt ) {
 	grunt.loadNpmTasks( '@lodder/grunt-postcss' );
 	grunt.loadNpmTasks( 'grunt-contrib-compress' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
-	grunt.loadNpmTasks( 'grunt-babel' );
 
 
 	// Register tasks
 	grunt.registerTask( 'default', [
 		'css',
-		'babel',
 		'jshint',
 		'uglify'
 	]);
