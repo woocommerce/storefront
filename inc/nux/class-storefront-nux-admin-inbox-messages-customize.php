ceb97771bc2370439b9417e429e18e9031576cc8
budzanowski <bartosz.budzanowski@gmail.com>
2020-12-06 04:29:20 +0100

    Update inbox message to the new design.

--- inc/nux/class-storefront-nux-admin-inbox-messages-customize.php
+++ inc/nux/class-storefront-nux-admin-inbox-messages-customize.php
@@ -29,17 +29,9 @@ class Storefront_NUX_Admin_Inbox_Messages_Customize {
 	 * @return Note
 	 */
 	public static function get_note() {
-
-		$action = '';
-		if ( 'page' === get_option( 'show_on_front' ) ) {
-			$action = __( 'Apply the Storefront homepage template', 'storefront' );
-		} else {
-			$action = __( 'Create a homepage using Storefront\'s homepage template', 'storefront' );
-		}
-
 		$note = new Note();
 		$note->set_title( __( 'Design your store with Storefront🎨', 'storefront' ) );
-		$note->set_content( __( 'You\'ve set up WooCommerce, now it\'s time to give it some style! Let\'s get started by entering the Customizer and adding your logo.', 'storefront' ) );
+		$note->set_content( __( 'Visit the Storefront settings page to start setup and customization of your Shop.', 'storefront' ) );
 		$note->set_type( Note::E_WC_ADMIN_NOTE_INFORMATIONAL );
 		$note->set_name( self::NOTE_NAME );
 		$note->set_content_data( (object) array() );
@@ -47,24 +39,10 @@ class Storefront_NUX_Admin_Inbox_Messages_Customize {
 		$note->add_action(
 			'customize-store-with-storefront',
 			__( 'Let\'s go!', 'storefront' ),
-			self::get_customizer_url(),
+			admin_url( 'themes.php?page=storefront-welcome' ),
 			Note::E_WC_ADMIN_NOTE_ACTIONED,
 			true
 		);
 		return $note;
-	}
-
-
-	/**
-	 * Generate the url to the customizer.
-	 *
-	 * @return String customizer link.
-	 */
-	public static function get_customizer_url() {
-		$args = array( 
-			'sf_starter_content' => '1',
-			'return'             => rawurlencode( admin_url( 'themes.php?page=storefront-welcome' ) )
-		);
-		return add_query_arg( $args, admin_url( 'customize.php' ) );
 	}
 }
