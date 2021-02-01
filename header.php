69bb7d504da99838cfe6fbbd1be8cd33e443381a
Siddharth <siddharthmani2000@gmail.com>
2021-02-01 17:05:48 +0530

    Fixes #1599 (#1600)

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2.0">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
