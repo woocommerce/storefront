8694a0f069494e4149ff4b1dc2d7cb88c0b561e5
radex02 <50751742+radex02@users.noreply.github.com>
2021-05-01 10:09:04 -0400

    "breadcrumbs" -> "Breadcrumb"

--- inc/woocommerce/class-storefront-woocommerce.php
+++ inc/woocommerce/class-storefront-woocommerce.php
@@ -223,7 +223,7 @@ if ( ! class_exists( 'Storefront_WooCommerce' ) ) :
 		 */
 		public function change_breadcrumb_delimiter( $defaults ) {
 			$defaults['delimiter']   = '<span class="breadcrumb-separator"> / </span>';
-			$defaults['wrap_before'] = '<div class="storefront-breadcrumb"><div class="col-full"><nav aria-label="breadcrumbs" class="woocommerce-breadcrumb">';
+			$defaults['wrap_before'] = '<div class="storefront-breadcrumb"><div class="col-full"><nav aria-label="Breadcrumb" class="woocommerce-breadcrumb">';
 			$defaults['wrap_after']  = '</nav></div></div>';
 			return $defaults;
 		}
