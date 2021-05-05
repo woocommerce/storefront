daf37b80628a05b21e11bcbae7a592a7449a2d47
Mike Jolley <mike.jolley@me.com>
2021-05-05 13:43:24 +0100

    Add configs

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	extends: [
+		'plugin:@woocommerce/eslint-plugin/recommended',
+	],
+	globals: {
+		jQuery: 'readonly',
+	},
+	settings: {},
+	rules: {
+		'woocommerce/feature-flag': 'off',
+		'@wordpress/no-global-active-element': 'warn',
+	},
+};
