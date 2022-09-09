4fab27fec5e757f744f1a3ed10c120a86ed9c602
Daniel W. Robert <danielwrobert@gmail.com>
2022-09-09 15:14:44 -0400

    Refactor to use :has() selector.
    Instead of directly targeting the Featured Items blocks, we can use the
    `:has()` selector to omit blocks that contain a child div with the
    `has-background-dim` class in the editor.
    
    Since browser support is not to where we need at the time of publishing
    this, we can also add a temp fallback with `@supports`.

--- inc/customizer/class-storefront-customizer.php
+++ inc/customizer/class-storefront-customizer.php
@@ -1254,8 +1254,14 @@ if ( ! class_exists( 'Storefront_Customizer' ) ) :
 				/* WP <=5.3 */
 				.editor-styles-wrapper .editor-block-list__block,
 				/* WP >=5.4 */
-				.editor-styles-wrapper .block-editor-block-list__block:not(.wp-block-woocommerce-featured-product):not(.wp-block-woocommerce-featured-category) {
+				.editor-styles-wrapper .block-editor-block-list__block:not(:has(div.has-background-dim)) {
 					color: ' . $storefront_theme_mods['text_color'] . ';
+				}
+				/* This following ruleset is a fallback for browsers that do not support the :has() selector. It can be removed once support reaches our requirements. */
+				@supports not (selector(:has(*))) {
+					.editor-styles-wrapper .block-editor-block-list__block:not(.wp-block-woocommerce-featured-product, .wp-block-woocommerce-featured-category) {
+						color: ' . $storefront_theme_mods['text_color'] . ';
+					}
 				}
 
 				.editor-styles-wrapper a,
