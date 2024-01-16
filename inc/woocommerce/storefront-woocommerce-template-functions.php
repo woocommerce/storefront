e745f93a82d4f10df9c510cc2e72e5b45d15168d
Tung Du <dinhtungdu@gmail.com>
2024-01-16 11:40:07 +0700

    Fix: allow svg in storefront messages, such as notices (#2133)

--- inc/woocommerce/storefront-woocommerce-template-functions.php
+++ inc/woocommerce/storefront-woocommerce-template-functions.php
@@ -235,7 +235,34 @@ if ( ! function_exists( 'storefront_shop_messages' ) ) {
 	 */
 	function storefront_shop_messages() {
 		if ( ! is_checkout() ) {
-			echo wp_kses_post( storefront_do_shortcode( 'woocommerce_messages' ) );
+			$kses_defaults = wp_kses_allowed_html( 'post' );
+
+			$svg_args = array(
+				'svg'   => array(
+					'class'           => true,
+					'aria-hidden'     => true,
+					'aria-labelledby' => true,
+					'role'            => true,
+					'xmlns'           => true,
+					'width'           => true,
+					'height'          => true,
+					'viewbox'         => true,
+				),
+				'g'     => array(
+					'fill' => true,
+				),
+				'title' => array(
+					'title' => true,
+				),
+				'path'  => array(
+					'd'    => true,
+					'fill' => true,
+				),
+			);
+
+			$allowed_tags = array_merge( $kses_defaults, $svg_args );
+
+			echo wp_kses( storefront_do_shortcode( 'woocommerce_messages' ), $allowed_tags );
 		}
 	}
 }
