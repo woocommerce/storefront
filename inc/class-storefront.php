940d937f2def95f2b362be6696c02e11fd8469c3
Luigi Teschio <gigitux@gmail.com>
2024-05-14 18:09:37 +0200

    remove site-logo support

--- inc/class-storefront.php
+++ inc/class-storefront.php
@@ -152,20 +152,6 @@ if ( ! class_exists( 'Storefront' ) ) :
 				)
 			);
 
-			/**
-			 *  Add support for the Site Logo plugin and the site logo functionality in JetPack
-			 *  https://github.com/automattic/site-logo
-			 *  http://jetpack.me/
-			 */
-			add_theme_support(
-				'site-logo',
-				apply_filters(
-					'storefront_site_logo_args',
-					array(
-						'size' => 'full',
-					)
-				)
-			);
 
 			/**
 			 * Declare support for title theme feature.
