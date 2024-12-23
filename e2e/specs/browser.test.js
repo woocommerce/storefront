0c0cb018195fa26b994f1581392292ea4c9a8241
Manish Menaria <the.manish.menaria@gmail.com>
2024-12-23 19:26:08 +0530

    Remove failing tests which was added for testing

--- e2e/specs/browser.test.js
+++ e2e/specs/browser.test.js
@@ -4,9 +4,7 @@ describe( 'Storefront', () => {
 	} );
 
 	it( 'should have "built with WooCommerce" footer', async () => {
-		const footerText = await page.evaluate(
-			() => document.querySelector( 'body' ).innerText
-		);
-		expect( footerText ).toMatch( 'This text does not exist' );
+		const footerText = await page.evaluate( () => document.querySelector( 'body' ).innerText );
+		expect( footerText ).toMatch( 'Built with WooCommerce.' );
 	} );
 } );
