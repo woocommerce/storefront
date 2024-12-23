f9c313862b5ebe1d4dfd849971b43e8e33ba58cb
Manish Menaria <the.manish.menaria@gmail.com>
2024-12-23 18:54:14 +0530

    Testing "E2E tests" workflow

--- e2e/specs/browser.test.js
+++ e2e/specs/browser.test.js
@@ -4,7 +4,9 @@ describe( 'Storefront', () => {
 	} );
 
 	it( 'should have "built with WooCommerce" footer', async () => {
-		const footerText = await page.evaluate( () => document.querySelector( 'body' ).innerText );
-		expect( footerText ).toMatch( 'Built with WooCommerce.' );
+		const footerText = await page.evaluate(
+			() => document.querySelector( 'body' ).innerText
+		);
+		expect( footerText ).toMatch( 'This text does not exist' );
 	} );
 } );
